83fdc6c95271a19531607b9641a7ce26e1025a21
Conor Kirby <conorkirby1@gmail.com>
2023-12-10 21:11:44 +0000

    Fixed keyboard control of cnc parts

--- Assets/Scripts/CNCMotionAgent.cs
+++ Assets/Scripts/CNCMotionAgent.cs
@@ -18,7 +18,7 @@ public class CNCMotionAgent : Agent
 	public bool trainingMode;
 
 	[Tooltip("Maximum force that motors can apply to move each axis")]
-	public float moveForce = 2f;
+	public float moveForce = 0.2f;
 
 	[Tooltip("XY acceleration (mm/sec^2)")]
 	public float xyAcceleration = 240;
@@ -35,7 +35,7 @@ public class CNCMotionAgent : Agent
 	private int stepsPerMM = 1600;
 	private int stepsPerRev = 200;
 	private int microStepsPerFullStep = 32;
-	private int mmPerRev = 4; 
+	private int mmPerRev = 4;
 	private float mmPerStep = 0.005f;
 	private float motorMaxTorque = 0.25f; //Nm
 	private float motorMaxVoltage = 12; //V
@@ -66,7 +66,7 @@ public class CNCMotionAgent : Agent
 		yGameObject = GameObject.Find("YMotion");
 		zGameObject = GameObject.Find("ZMotion");
 
-		xRigidBody = xGameObject.GetComponent<Rigidbody>();
+        xRigidBody = xGameObject.GetComponent<Rigidbody>();
 		yRigidBody = yGameObject.GetComponent<Rigidbody>();
 		zRigidBody = zGameObject.GetComponent<Rigidbody>();
 		contactStickRigidBody = contactStickGameObject.GetComponent<Rigidbody>();
@@ -81,6 +81,25 @@ public class CNCMotionAgent : Agent
 	}
 
 	/// <summary>
+	/// Called when action is received from plauyer or NN
+	/// 
+	/// We have a vector of actions 
+	/// Index 0: move X stage (+1 right, -1 left)
+	/// Index 1: move y stage (+1 towards back, -1 towards front)
+	/// Index 2: move z stage (+1 up, -1 down)
+	/// </summary>
+	/// <param name="actions"></param>
+	public override void OnActionReceived(ActionBuffers actions)
+	{
+		if (frozen) return; // Used only after training
+
+		xRigidBody.AddForce(Vector3.right * actions.ContinuousActions[0] * moveForce);
+		yRigidBody.AddForce(Vector3.forward * actions.ContinuousActions[1] * moveForce);
+		zRigidBody.AddForce(Vector3.up * actions.ContinuousActions[2] * moveForce);
+
+	}
+
+	/// <summary>
 	/// When behaviour set to heuristic only, this func called.
 	/// Return values fed into <see cref="OnActionReceived(ActionBuffers)"/>
 	/// instead of NN
@@ -89,31 +108,25 @@ public class CNCMotionAgent : Agent
 	public override void Heuristic(in ActionBuffers actionsOut)
 	{
 		// Use the keyboard to move each platform
-		Vector3 xMotion = Vector3.zero;
-		Vector3 yMotion = Vector3.zero;
-		Vector3 zMotion = Vector3.zero;
+		int xMotion = 0;
+		int yMotion = 0;
+		int zMotion = 0;
 
-		if (Input.GetKey(KeyCode.W));
-		else if (Input.GetKey(KeyCode.S));
+		if (Input.GetKey(KeyCode.W)) yMotion = -1;
+		else if (Input.GetKey(KeyCode.S)) yMotion = 1;
+		else yMotion = 0;
 
-		if (Input.GetKey(KeyCode.A));
-		else if (Input.GetKey(KeyCode.F));
+		if (Input.GetKey(KeyCode.A)) xMotion = 1;
+		else if (Input.GetKey(KeyCode.D)) xMotion = -1;
+		else xMotion = 0;
 
-		if (Input.GetKey(KeyCode.E));
-		else if (Input.GetKey(KeyCode.C));
+		if (Input.GetKey(KeyCode.E)) zMotion = 1;
+		else if (Input.GetKey(KeyCode.F)) zMotion = -1;
+		else zMotion = 0;
 
-		if (Input.GetKey(KeyCode.UpArrow));
-		else if (Input.GetKey(KeyCode.DownArrow));
-
-		if (Input.GetKey(KeyCode.LeftArrow));
-		else if (Input.GetKey(KeyCode.RightArrow));
-
-		//Vector3 combined = (forward + left + up).normalized;
-
-		//var continActionsOut = actionsOut.ContinuousActions;
-		//continActionsOut[0] = combined.x;
-		//continActionsOut[1] = combined.y;
-		//continActionsOut[2] = combined.z;
-		//continActionsOut[3] = pitch;
-		//continActionsOut[4] = yaw;
+		var continActionsOut = actionsOut.ContinuousActions;
+		continActionsOut[0] = xMotion;
+		continActionsOut[1] = yMotion;
+		continActionsOut[2] = zMotion;
 	}
+}
